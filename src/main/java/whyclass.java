f0e71650474cf8b5f4444aba059af1b69dc5262b
Aleksey <alekseyh215@gmail.com>
2019-10-17 14:27:49 +0400

    switch, case, break loops,3x3

--- src/main/java/whyclass.java
+++ src/main/java/whyclass.java
@@ -15,10 +15,73 @@ public class whyclass {
                 break;
         }
 
+int money = 50_000;
+
+    }
+
+    public static void same(String[] args) {
+        for(int i=10; i<20; i++){
+            System.out.println(Math.round(i));
+
+        }
 
 
     }
 
+    public static void hgvymain(String args[])
+    {
+
+        int i, j, k, space=10;
+
+        for(i=0; i<10; i++)
+        {
+            for(k=0; k<space; k++)
+            {
+                System.out.print(" ");
+            }
+
+            for(j=0; j<i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+            space--;
+        }
+
+    }
+
+
+    public static void msain(String[] args) {
+
+
+        int[][][] c = new int[0][0][0];
+        
+        System.out.println(c.length);
+        int[][] d = c[0];
+
+        for(int i=1; i<c.length; i++){
+            for (int j=0; j<c[j].length; j++){
+                for (int k=0; k<c[i][j].length; k++){
+                    System.out.printf("%3d", c[i][j][k]);
+                }
+                System.out.println(" ");
+            }
+            System.out.println(" ");
+
+        }
+
+
+
+    }
+
+    public static void dwqdmain(String[] args) {
+        for (int i=0; i<10; i++){
+            System.out.println(i);
+        }
+    }
+
+    //ascii
 
 
 

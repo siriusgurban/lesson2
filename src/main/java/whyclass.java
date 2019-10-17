9aaaeec726d64df804ab5f6b47ea9e365cb26224
Aleksey <alekseyh215@gmail.com>
2019-10-17 11:33:41 +0400

    switch, case, break

--- src/main/java/whyclass.java
+++ src/main/java/whyclass.java
@@ -2,7 +2,29 @@ public class whyclass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        int z=5;
+        switch (z){
+            case 1:
+                System.out.println("1");
+                break;
+            case 5:
+                System.out.println("5");
+                break;
+            case 12:
+                System.out.println("12");
+                break;
+        }
+
+
+
     }
+
+
+
+
+
+
+
 }
 
 
